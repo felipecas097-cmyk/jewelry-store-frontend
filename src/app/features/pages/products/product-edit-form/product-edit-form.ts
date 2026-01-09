028ecfd2c36d3d5a9ca84970be70f3804f274b2c
Ronald <guerrerotoloza.ro@gmail.com>
2026-01-08 21:58:36 -0500

    [+] Se agrego entidad Products(list/edit/new) y rutas / [+] Se agrego el menu navegable

---
+++ src/app/features/pages/products/product-edit-form/product-edit-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-edit-form',
+  imports: [],
+  templateUrl: './product-edit-form.html',
+  styleUrl: './product-edit-form.css',
+})
+export class ProductEditForm {
+
+}
