4cfb5740a8f3e32ff0dacd27bf9fb8e706b438ff
Ronald <guerrerotoloza.ro@gmail.com>
2026-02-05 10:03:38 -0500

    [+] Mejore el diseño de el formulario de creacion de los productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 /* To learn more about Angular compiler options: https://angular.dev/reference/configs/angular-compiler-options. */
 import { Routes } from '@angular/router';
 import { Home } from './features/pages/home/home';
-import { Login } from './features/pages/login/login';
-import { Register } from './features/pages/register/register';
 import { ErrorPageNotFound } from './features/pages/error-page-not-found/error-page-not-found';
 import { ProductList } from './features/pages/products/product-list/product-list';
 import { ProductEditForm } from './features/pages/products/product-edit-form/product-edit-form';
@@ -10,11 +8,9 @@ import { ProductNewForm } from './features/pages/products/product-new-form/produ
 
 export const routes: Routes = [
     { path: `home`, component: Home}, 
-    { path: `login`, component: Login},
     { path: `dashboard/products`, component: ProductList},
     { path: `dashboard/products/edit`, component: ProductEditForm},
     { path: `dashboard/products/new`, component: ProductNewForm},
-    { path: `register`, component: Register},
     { path: `404`, component: ErrorPageNotFound},
     { path: ``, redirectTo: `home`, pathMatch: `full`},
     { path: `**`, redirectTo: `404`, pathMatch: `full`} 
