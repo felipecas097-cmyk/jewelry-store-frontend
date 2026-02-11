553293faa7156c156fd901cfa7de679ab008d6d7
felipe cardenas <felipecas097@gmail.com>
2026-02-10 21:49:29 -0500

    se creo el despliegue dinamico de los productos filtrados por su categoria

--- src/app/core/services/http-products.ts.ts
+++ src/app/core/services/http-products.ts.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,10 @@ import { inject, Injectable } from '@angular/core';
 export class HttpProduct {
   private http = inject(HttpClient);
 
-  // constructor( private http: HttpClient ) {}
+  getProductsGroupedByCategory() {
+    return this.http.get<any>('http://localhost:3000/api/v1/product/category').pipe(
+      tap( data => console.log( data ) ),
+      map( data => data.products )
+    )
+  }
 }
--- src/app/features/pages/collections-new-form/collections-new-form.ts
+++ src/app/features/pages/collections-new-form/collections-new-form.ts
@@ -1,16 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpCollections } from '../../../core/services/http-collections';
 import { map, switchMap } from 'rxjs/operators';
 
-
-
-
+import { HttpProduct } from '../../../core/services/http-products.ts';
 
 @Component({
   selector: 'app-collections-new-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe, JsonPipe],
   templateUrl: './collections-new-form.html',
   styleUrl: './collections-new-form.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,25 +16,54 @@ import { map, switchMap } from 'rxjs/operators';
 export class CollectionsNewForm {
   formData!: FormGroup;
 
-  public collections: Observable <any[]> = new Observable <any[]>(); 
+  public products: Observable <{}> =new Observable <{}>();
 
-  private refreshCollectionsTrigger$: BehaviorSubject <void> = new 
-  BehaviorSubject<void>(undefined);
-  
+  private refreshProductsTrigger$: BehaviorSubject <void> = new
+  BehaviorSubject <void>(undefined);
 
-  constructor( 
-    private httpCollections: HttpCollections ) {
+  constructor(  
+    private httpProducts: HttpProduct
+  ) {
     this.formData = new FormGroup({
       name: new FormControl ('', [Validators.required, Validators.minLength(3)]),
       description: new FormControl ('',[Validators.required,Validators.minLength(10)]),
+      products: new FormArray([]),
       isActive: new FormControl (true),
     });
   }
 
 
  ngOnInit(): void {
-  this.collections = this.refreshCollectionsTrigger$.pipe(
-    switchMap( () => this.httpCollections.getAllCollection() )
+  
+  this.products = this.refreshProductsTrigger$.pipe (
+    switchMap(() => this.httpProducts.getProductsGroupedByCategory()  ),
+    map(data =>
+      Object.entries(data).map(([key, value]) => ({ key, value }))
+    )
   )
+
  }
+
+ get productsArray(): FormArray {
+  return this.formData.get('products') as FormArray;
+}
+
+onCheckboxChange(event: any, productId: string) {
+
+  if (event.target.checked) {
+    this.productsArray.push(new FormControl(productId));
+  } else {
+
+    const index = this.productsArray.controls
+      .findIndex(x => x.value === productId);
+
+    this.productsArray.removeAt(index);
+  }
+
+}
+
+ onSubmit () {
+  console.log(this.formData.value)
+ }
+
 }
