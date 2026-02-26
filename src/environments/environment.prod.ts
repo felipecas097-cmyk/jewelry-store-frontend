1595fd431e3bcc793348c2daf8bf8466fce70d5e
Ronald <guerrerotoloza.ro@gmail.com>
2026-02-26 15:52:56 -0500

    chore(environments): set public AWS IP for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // IMPORTANTE: Aquí va la IP PÚBLICA (o el dominio) de tu servidor AWS
   // Ejemplo: 'http://54.123.45.67:3000/api/v1'
-  apiUrl: 'http://TU_IP_PUBLICA_AQUI:3000/api/v1',
+  apiUrl: 'http://18.220.176.111:3000/api/v1',
 };
